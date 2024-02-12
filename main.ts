9d2d7994fe155be74f57edd5554f55336a8a407a
AhmedQamar7 <mahmedkinga301@gmail.com>
2024-02-12 09:59:30 +0500

    calculator

---
+++ main.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+  {
+    type: "number",
+    name: "numberOne",
+    message: "kindly enter your first"
+  },
+  {
+    type: "number",
+    name: "numberTwo",
+    message: "kindly enter your second number"
+  },
+  {
+    type: "list",
+    name: "Operator",
+    choices: ["addition(+)", "subtraction(-)", "multiply(*)", "division(/)"],
+    message: "select your operator."
+  }
+]);
+
+console.log(answer)
+
+const {numberOne,numberTwo,Operator} = answer;
+
+if(numberOne && numberTwo && Operator){
+  let result:number=0;
+
+  if (Operator === "addition(+)"){
+    result=numberOne + numberTwo;
+  }
+  else if (Operator === "subtraction(-)"){
+    result=numberOne - numberTwo;
+  }
+  else if (Operator === "multiply(*)"){
+    result=numberOne*numberTwo;
+  }
+  else if (Operator === "division(/)"){
+    result=numberOne/numberTwo;
+  }
+  else {
+    console.log("Kindly enter valid input")
+  }
+
+  console.log("Your result is:", result)
+}
